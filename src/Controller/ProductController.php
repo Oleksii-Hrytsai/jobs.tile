826addc916f19790f091149c3f5db7755b15d1dc
Oleksii-Hrytsai <alexey.gritsaiy@gmail.com>
2024-05-06 08:56:46 +0300

    add SoapController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,16 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+        $this->httpClient = $httpClient;
+    }
+    #[Route('/product', name: 'app_product')]
+    public function index(Request $request): JsonResponse
+    {
+        $factory = $request->query->get('factory');
+        $collection = $request->query->get('collection');
+        $article = $request->query->get('article');
+
+        $url = "https://tile.expert/fr/tile/{$factory}/{$collection}/a/{$article}";
+        $response = $this->httpClient->request('GET', $url);
+        $crawler = new Crawler($response->getContent());
+        $price = $crawler->filter('.price-tag')->text(); // Припустимо, що ціна знаходиться в елементі з класом 'price-tag'
+
+        return new JsonResponse([
+            'price' => $price,
+            'factory' => $factory,
+            'collection' => $collection,
+            'article' => $article
         ]);
     }
 }
