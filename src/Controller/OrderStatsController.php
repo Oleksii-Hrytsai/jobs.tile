d2d22a34cbb4f7cada95565052bce9b47691a057
Oleksii-Hrytsai <alexey.gritsaiy@gmail.com>
2024-05-07 09:09:51 +0300

    2 step complete

--- src/Controller/OrderStatsController.php
+++ src/Controller/OrderStatsController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class OrderStatsController extends AbstractController
 {
-    #[Route('/order/stats', name: 'app_order_stats')]
-    public function index(): Response
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('order_stats/index.html.twig', [
-            'controller_name' => 'OrderStatsController',
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/orders/stats', name: 'app_order_stats')]
+    public function getOrderStats(Request $request): JsonResponse
+    {
+        $page = (int) $request->query->get('page', 1);
+        $limit = (int) $request->query->get('limit', 10);
+        $groupBy = $request->query->get('groupBy', 'month');
+
+        $queryBuilder = $this->entityManager->createQueryBuilder()
+            ->select("COUNT(o.id) as orderCount, SUBSTRING(o.orderDate, 1, {$this->getSubstringLength($groupBy)}) as groupedDate")
+            ->from(Order::class, 'o')
+            ->groupBy('groupedDate')
+            ->setFirstResult(($page - 1) * $limit)
+            ->setMaxResults($limit);
+
+        $query = $queryBuilder->getQuery();
+        $result = $query->getResult();
+
+        return new JsonResponse([
+            'data' => $result,
+            'page' => $page,
+            'limit' => $limit,
+            'totalPages' => ceil(count($result) / $limit)
         ]);
     }
+
+    private function getSubstringLength($groupBy): int
+    {
+        return ['year' => 4, 'month' => 7, 'day' => 10][$groupBy] ?? 7; // Default to month
+    }
 }
+
