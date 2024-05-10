d8cf629540124b5d04868a41da091d3ccab873bc
Oleksii-Hrytsai <alexey.gritsaiy@gmail.com>
2024-05-10 11:29:49 +0300

    change namespace has been conflict

--- tests/Controllers/ProductControllerTest.php
+++ tests/Controllers/ProductControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Controllers;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
